062a2c540b4c806d6f6afa4d52eb6887cc4ee3b6
S:Ddhant Jain <jainsid123456789@gmail.com>
2021-11-21 15:16:37 -0500

    13 ms

---
+++ solutions/819. Most Common Word.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Set<String> bannedWords = new HashSet<>();
+        Map<String, Integer> wordCount = new HashMap<>();
+        String[] words = paragraph.replaceAll("[^A-Za-z]"," ").toLowerCase().split(" ");
+​
+        for(String word: banned) {
+            bannedWords.add(word);
+        }
+​
+        for(String word: words) {
+            if(!bannedWords.contains(word)) {
+                wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+            }
+        }
+        System.out.println(wordCount);
+​
+        String result = "";
+        for(String word: wordCount.keySet()) {
+            if(result.equals("")) {
+                result = word;
+            }
+            else{
+                if(wordCount.get(result) > wordCount.get(word)){
+                    result = result;
+                }else{
+                    result = word;
+                }
+            }
+        }
+        return result;
+    }
+}
